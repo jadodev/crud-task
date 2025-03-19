bbd12c9f6273af3e66d4950ee80b391db7edc550
Jonathan Urrego <jadougdev@gmail.com>
2025-03-19 14:39:54 -0500

    port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { NotFoundFilter } from './middleware/NotFoundFilter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new NotFoundFilter());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000,'0.0.0.0');
 }
 bootstrap();
