0768e5045a4970c7cea3baaeb78a3f0a9a6990c1
Jonathan Urrego <jadougdev@gmail.com>
2025-03-19 14:52:19 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { NotFoundFilter } from './middleware/NotFoundFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalFilters(new NotFoundFilter());
   await app.listen(3000,'0.0.0.0');
 }
